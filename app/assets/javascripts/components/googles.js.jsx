f3043b83f6e4dcb7aef22421a2c58a2a7205ae0a
GavMan1995 <gavyncaldwell@gmail.com>
2016-04-11 13:50:42 -0600

    multiple xearch params

--- app/assets/javascripts/components/googles.js.jsx
+++ app/assets/javascripts/components/googles.js.jsx
@@ -1,15 +1,31 @@
 class GoogleSearch extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {search: "", results: []}
-    this.startSearch = this.startSearch.bind(this);
+    this.state = {search: "", results: [], price: "1", distance: "1609.34", foodInputs: 1}
+    this.setPrice = this.setPrice.bind(this);
+    this.setDistance = this.setDistance.bind(this);
+    this.searchFields = this.searchFields.bind(this);
+    this.getSpots = this.getSpots.bind(this);
   }
 
-  getSpots() {
+  getSpots(e) {
+    e.preventDefault();
+
+    let searchValue = '';
+    for(let i = 0; i < Object.keys(this.refs).length; i++){
+      if(i == Object.keys(this.refs).length - 1) {
+        searchValue += `${this.refs[Object.keys(this.refs)[i]].value.trim()}`
+      }
+     else {
+       searchValue += `${this.refs[Object.keys(this.refs)[i]].value.trim()}|`;
+     }
+    }
+
+    console.log(searchValue);
     $.ajax({
       url: '/search',
       type: 'GET',
-      data: {search: this.state.search}
+      data: {search: searchValue, price: this.state.price, distance: this.state.distance}
     }).done(data => {
       this.setState({results: data, search: ""})
     }).fail(data => {
@@ -18,41 +34,60 @@ class GoogleSearch extends React.Component {
 
   }
 
-  startSearch(e) {
-    e.preventDefault();
-    this.setState({
-      search: this.refs.search.value
-    });
-  }
-
-  newSearch() {
-    if(this.state.search != ""){
-      this.getSpots();
-    }
-  }
-
   results() {
+    let price = parseInt(this.state.price)
     if(this.state.results != []){
       return this.state.results.map( result => {
-        return(
-          <div>
-            <h1>{result.name}</h1>
-            <h3>{result.formatted_address}</h3>
-          </div>
-        );
+        if (result.price_level <= price) {
+          return(
+            <div>
+              <h1>Name: {result.name}</h1>
+              <h3>Address: {result.vicinity}</h3>
+              <h3>Price: {result.price_level}</h3>
+            </div>
+          );
+        }
       });
     }
+  }
+
+  setPrice(e) {
+    let value = e.target.value;
+    this.setState({price: value});
+  }
+
+  setDistance(e) {
+    let value = e.target.value;
+    this.setState({distance: value});
+  }
+
+  searchFields() {
+    let foodInputs = [];
+    for(let i = 0; i < this.state.foodInputs; i++) {
+      foodInputs.push(<div><input key={`food-input-${i}`} type="text" placeholder="search for food" ref={`search-${i}`} /></div>)
+    }
+    return(foodInputs);
   }
 
 
   render() {
     return(
       <div>
-        <form onSubmit={this.startSearch}>
-          <input type="text" placeholder="search for food" ref="search"/>
+        <form onSubmit={this.getSpots}>
+          {this.searchFields()}
+          <br/>
+            <input type="radio" name="price" value="1" defaultChecked onChange={this.setPrice}/> $
+            <input type="radio" name="price" value="2" onChange={this.setPrice}/> $$
+            <input type="radio" name="price" value="3" onChange={this.setPrice}/> $$$
+          <br/>
+            <input type="radio" name="distance" value="1609.34" defaultChecked onChange={this.setDistance}/> walking
+            <input type="radio" name="distance" value="8046.72" onChange={this.setDistance}/> 5 miles
+            <input type="radio" name="distance" value="32186.9" onChange={this.setDistance}/> 20 miles
+            <input type="radio" name="distance" value="80467.2" onChange={this.setDistance}/> 50 miles
+          <br/>
           <button type="submit">Search</button>
+          <button type='button' onClick={() => this.setState({foodInputs: this.state.foodInputs + 1})}>Add More Food</button>
         </form>
-        {this.newSearch()}
         {this.results()}
       </div>
     )
--- app/controllers/google_controller.rb
+++ app/controllers/google_controller.rb
@@ -4,15 +4,15 @@ class GoogleController < ApplicationController
     end
 
     def search
-      @spots = @client.spots_by_query(params["search"])
+      price = params["price"]
+      distance = params["distance"].to_i
+      lat = "40.7608".to_f
+      long = "-111.8910".to_f
+      search = params["search"]
+      @spots = @client.spots(lat, long, :radius =>  distance, :name => search, :types => ['restaurant', 'food', 'meal_takeaway', 'meal_delivery', 'cafe', 'bakery'] )
       render json: @spots
     end
 
-    private
-
-    def client
-      @client = GooglePlaces::Client.new(ENV['API_KEY'])
-    end
 end
 
 # , :types => ['restaurant', 'food', 'meal_takeaway', 'meal_delivery', 'cafe', 'bakery']
